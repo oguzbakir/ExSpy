f31dc3af27e20bf19f1860bffa7e660d021f2507
oguzbakir <b21627007@cs.hacettepe.edu.tr>
2018-07-28 23:23:14 +0300

    added enumerate and stdout to list

--- core.py
+++ core.py
@@ -1,17 +1,32 @@
+#!/usr/bin/env python3
 import os
 import sys
-from subprocess import call
+import subprocess
+from enum import Enum
 
+Type = Enum('Type', 'A a B b D d f L l T t U')
+
+fileName = "ex"
+functions = []
 
 class Function:
-    address = 0
+    address = ""
     size = 0
-    type = ""
+    function_t = Type.U
     name = ""
 
-    def __init__(self, address, size, type, name):
+    def __init__(self, address, size, function_t, name):
         self.address = address
         self.size = size
-        self.type = type
+        self.function_t = function_t
         self.name = name
+
+
+stdout = subprocess.check_output(["nm", "-C", "--print-size", "--size-sort", fileName]).decode("utf-8")
+
+
+for line in stdout.split("\n"):
+    elem = line.split(" ")
+    if len(elem) == 4:
+        functions.append(Function(elem[0], elem[1], elem[2], elem[3]))
 
